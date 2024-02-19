e2bd7f397dd485f73359bdbcbae9c4a6eee1b2de
tubame <25952690+tubame0505@users.noreply.github.com>
2024-02-19 23:24:20 +0900

    python 3.11-slim

---
+++ app/hello.py
@@ -0,0 +1,15 @@
+import os
+
+
+def get_cwd() -> None:
+    return os.getcwd()
+
+
+if __name__ == "__main__":
+    print(get_cwd())
+
+
+x = ()
+
+a = x[(1 + 1) : 5]
+print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
